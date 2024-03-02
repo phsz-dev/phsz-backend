d1b70db82a69c87e27ef5e1cd397bb0962204808
lxy <lv2098793146@126.com>
2024-03-02 11:04:40 +0800

    init
    Signed-off-by: lxy <lv2098793146@126.com>

---
+++ src/main/java/com/example/phszbackend/PhszBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.phszbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PhszBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PhszBackendApplication.class, args);
+	}
+
+}
