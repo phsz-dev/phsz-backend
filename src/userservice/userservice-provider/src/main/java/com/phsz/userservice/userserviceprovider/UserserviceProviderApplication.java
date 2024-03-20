628fd16dd590924555cee4b5ddb1153e1e2e0b9d
ALiersEL <johnathansteinfeld@gmail.com>
2024-03-20 14:31:33 +0800

    chore: update project structure and add JPA dependency

---
+++ src/userservice/userservice-provider/src/main/java/com/phsz/userservice/userserviceprovider/UserserviceProviderApplication.java
@@ -0,0 +1,7 @@
+package com.phsz.userservice.userserviceprovider;
+
+public class UserserviceProviderApplication {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
