9c870a7816b7af5c6c7890d98b8a28d76888766d
yhzx233 <2058777361@qq.com>
2024-04-02 22:24:09 +0800

    fix: authentication

--- src/gateway/src/main/java/com/phsz/gateway/JwtAuthenticationFilter.java
+++ src/gateway/src/main/java/com/phsz/gateway/JwtAuthenticationFilter.java
@@ -5,6 +5,8 @@ import com.nimbusds.jose.JWSVerifier;
 import com.nimbusds.jose.crypto.MACVerifier;
 import com.nimbusds.jwt.JWTClaimsSet;
 import com.nimbusds.jwt.SignedJWT;
+import org.springframework.cloud.gateway.filter.GatewayFilterChain;
+import org.springframework.cloud.gateway.filter.GlobalFilter;
 import org.springframework.core.annotation.Order;
 import org.springframework.core.io.buffer.DataBuffer;
 import org.springframework.core.io.buffer.DataBufferFactory;
@@ -12,17 +14,17 @@ import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.server.reactive.ServerHttpRequest;
+import org.springframework.stereotype.Component;
 import org.springframework.web.server.ServerWebExchange;
-import org.springframework.web.server.WebFilter;
-import org.springframework.web.server.WebFilterChain;
 import reactor.core.publisher.Mono;
 
 import java.text.ParseException;
 import java.util.Base64;
 import java.util.Date;
 
-@Order(-1)
-public class JwtAuthenticationFilter implements WebFilter {
+@Component
+@Order(1)
+public class JwtAuthenticationFilter implements GlobalFilter {
 
 	private final JWSVerifier verifier;
 
@@ -32,33 +34,42 @@ public class JwtAuthenticationFilter implements WebFilter {
     }
 
     @Override
-	public Mono<Void> filter(ServerWebExchange exchange, WebFilterChain chain) {
+	public Mono<Void> filter(ServerWebExchange exchange, GatewayFilterChain chain) {
+		String path = exchange.getRequest().getURI().getPath();
+		if (permitAll(path)) {
+			return chain.filter(exchange);
+		}
 		// 从HTTP请求中获取JWT
 		String token = extractJwtFromRequest(exchange.getRequest());
-		JWTClaimsSet claims = getClaimsFromJwt(token);
-		if (token != null && claims != null && validateJwt(token)) {
-			try {
-				ServerHttpRequest request = exchange.getRequest().mutate()
-						.header("Username", claims.getSubject())
-						.header("Roles", String.join(",", claims.getStringListClaim("roles")))
-						.build();
-				return chain.filter(exchange.mutate().request(request).build());
-			} catch (ParseException e) {
-				throw new RuntimeException(e);
+		if (token != null && validateJwt(token)) {
+			JWTClaimsSet claims = getClaimsFromJwt(token);
+			if (claims != null) {
+				try {
+					ServerHttpRequest request = exchange.getRequest().mutate()
+							.header("Username", claims.getSubject())
+							.header("Roles", String.join(",", claims.getStringListClaim("roles")))
+							.build();
+					return chain.filter(exchange.mutate().request(request).build());
+				} catch (ParseException e) {
+					throw new RuntimeException(e);
+				}
 			}
-        } else {
-			// 如果JWT无效，设置HTTP状态码为401
-			exchange.getResponse().setStatusCode(HttpStatus.UNAUTHORIZED);
-			// 设置内容类型为application/json
-			exchange.getResponse().getHeaders().setContentType(MediaType.APPLICATION_JSON);
-			// 创建返回的错误消息
-			String errorMessage = "{\"error\": \"Unauthorized\", \"message\": \"Invalid JWT token\"}";
-			// 获取DataBufferFactory来创建DataBuffer
-			DataBufferFactory dataBufferFactory = exchange.getResponse().bufferFactory();
-			DataBuffer buffer = dataBufferFactory.wrap(errorMessage.getBytes());
-			// 使用writeWith方法返回错误消息
-			return exchange.getResponse().writeWith(Mono.just(buffer));
-		}
+        }
+		// 如果JWT无效，设置HTTP状态码为401
+		exchange.getResponse().setStatusCode(HttpStatus.UNAUTHORIZED);
+		// 设置内容类型为application/json
+		exchange.getResponse().getHeaders().setContentType(MediaType.APPLICATION_JSON);
+		// 创建返回的错误消息
+		String errorMessage = "{\"error\": \"Unauthorized\", \"message\": \"Invalid JWT token\"}";
+		// 获取DataBufferFactory来创建DataBuffer
+		DataBufferFactory dataBufferFactory = exchange.getResponse().bufferFactory();
+		DataBuffer buffer = dataBufferFactory.wrap(errorMessage.getBytes());
+		// 使用writeWith方法返回错误消息
+		return exchange.getResponse().writeWith(Mono.just(buffer));
+	}
+
+	private boolean permitAll(String path) {
+		return path.startsWith("/api/auth/");
 	}
 
 	private String extractJwtFromRequest(ServerHttpRequest request) {
