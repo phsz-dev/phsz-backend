7da0a11edd4363dc5e1fca87ee7c8be060beb86d
yhzx233 <2058777361@qq.com>
2024-04-08 18:22:00 +0800

    fix: JsonNode

--- src/chargeservice/chargeservice-provider/src/main/java/com/phsz/chargeservice/chargeserviceprovider/pojo/Charge.java
+++ src/chargeservice/chargeservice-provider/src/main/java/com/phsz/chargeservice/chargeserviceprovider/pojo/Charge.java
@@ -1,5 +1,6 @@
 package com.phsz.chargeservice.chargeserviceprovider.pojo;
 
+import com.fasterxml.jackson.databind.JsonNode;
 import com.phsz.common.JsonbConverter;
 import jakarta.persistence.*;
 import lombok.Data;
@@ -16,7 +17,7 @@ public class Charge {
     private Long chargeId;
     @Column(columnDefinition = "jsonb")
     @Convert(converter = JsonbConverter.class)
-    private String details; // 假设这是新增的字段，表示收费详情
+    private JsonNode details; // 假设这是新增的字段，表示收费详情
     private Date date; // 假设这是收费日期
 
     // 根据需要添加或修改字段
